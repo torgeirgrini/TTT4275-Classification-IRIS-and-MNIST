9bf3f754f2ee1e689bf93dddbd68ed568a7fcde8
Jon Torgeir Grini <torgeirgrini@gmail.com>
2022-04-07 01:35:16 +0200

    Task 1 complete

---
+++ Classification-MNIST/README.md
@@ -0,0 +1,6 @@
+1. pip install tensorflow
+2. pip install keras
+3. pip install numpy
+4. pip install matplotlib
+5. pip install sklearn
+
--- Classification-MNIST/mnist_task1.py
+++ Classification-MNIST/mnist_task1.py
@@ -1,80 +1,109 @@
-from turtle import distance
 from keras.datasets import mnist
 import numpy as np
-from matplotlib import pyplot
+from matplotlib import pyplot as plt
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import confusion_matrix
-from sklearn.model_selection import train_test_split
-from scipy.spatial import distance
+from sklearn import metrics #confusion_matrix, plot_confusion_matrix
+import timeit
 
-def prepareTrainingData(trainX, trainy, testX, testy, chunkSize):
-    trainingSetSize = trainX.shape[0]
-    imageSizeX = trainX.shape[1]
-    imageSizeY = trainX.shape[2]
-    numChunks = trainingSetSize // chunkSize
+def reshapeDataSet(trainX, trainy, testX, testy, numTrainChunks, numTestChunks): 
+    numTrainingSamples, imageSizeX, imageSizeY = trainX.shape
+    numTestSamples = len(testX)
     
-    trainX = np.array_split(trainX, numChunks)
-    trainy = np.array_split(trainy, numChunks)
-    testX = np.array_split(testX, numChunks)
-    testy = np.array_split(testy, numChunks)
+    trainX = trainX.reshape(numTrainingSamples, imageSizeX*imageSizeY)
+    testX = testX.reshape(numTestSamples, imageSizeX*imageSizeY)
+    
+    trainX = np.array_split(trainX, numTrainChunks)
+    trainy = np.array_split(trainy, numTrainChunks)
+    testX = np.array_split(testX, numTestChunks)
+    testy = np.array_split(testy, numTestChunks)
 
-    chunks = []
-    for i in range(0, len(trainX)):
-        chunks.append([(trainX[i], trainy[i]), (testX[i], testy[i])])
-    return chunks
+    trainingChunks, testingChunks = [], []
+    for i in range(numTrainChunks):
+        trainingChunks.append((trainX[i], trainy[i]))
+    for j in range(numTestChunks):
+        testingChunks.append((testX[j], testy[j]))
+        
+    return trainingChunks, testingChunks
     
 
 def knnEuclideanClassifier(trainX, trainy, testX, numNeighbors):
-    print(str(trainX.shape))
-    print(str(testX.shape))
-
-
-    trainX = trainX.reshape(len(trainX), len(trainX[0])*len(trainX[0][0]))
-    testX = testX.reshape(len(testX), len(testX[0])*len(testX[0][0]))
-    neigh = KNeighborsClassifier(n_neighbors=numNeighbors, metric = distance.euclidean)
+    neigh = KNeighborsClassifier(n_neighbors=numNeighbors) #euclidean distance by default
+    start = timeit.default_timer()
     neigh.fit(trainX, trainy)
-    print(str(trainX.shape))
-    print(str(trainX[0].shape))
-    print(str(testX.shape))
-    print(str(testX[0].shape))
-    return neigh.predict(testX)
+    calcTime = timeit.default_timer() - start
+    start = timeit.default_timer()
+    predy = neigh.predict(testX)
+    predTime = timeit.default_timer() - start
+    return predy, calcTime, predTime
     
     
-
-def main():
-    trainingSampleSize = 5000
-    testingSampleSize = 100
-    chunkSize = 1000
-    trainingIndex = 0 #number between 0 and (60000/chunksize - 1)
- 
+if __name__ == '__main__':
+    
     #Load the MNIST dataset
     (trainX, trainy), (testX, testy) = mnist.load_data()
+    numTrainingSamples, imageW, imageH = trainX.shape
+    numTestingSamples = len(testX)
+    
+    #Adjustable parameters
+    trainingSampleSize = 60000 #number between 0 and 60000
+    testingSampleSize = 10000 #number between 0 and 10000
+    trainChunkIndex = 0
+    testChunkIndex = 0
+    numNeighbors = 3
+    numPlotImages = 9
+
+    assert len(trainX) == len(trainy)
+    assert len(testX) == len(testy)
+    assert trainingSampleSize <= numTrainingSamples
+    assert testingSampleSize <= numTestingSamples
+    
+    numTrainChunks = numTrainingSamples//trainingSampleSize
+    numTestChunks = numTrainChunks
     
     #Need to preprocess the data by chunking it into sizes of chunkSize datapoints in each chunk
-    chunks = prepareTrainingData(trainX, trainy, testX, testy, chunkSize)
-    (trainX, trainy), (testX, testy) = chunks[trainingIndex]
-    
-    numNeighbors = 3
-    
-    labelPredictions = knnEuclideanClassifier(trainX, trainy, testX, numNeighbors)
-    
-    print(testy)
-    print(labelPredictions)
-    
+    trainChunks, testChunks = reshapeDataSet(trainX, trainy, testX, testy, numTrainChunks, numTestChunks)
+    (trainX, trainy), (testX, testy) = trainChunks[trainChunkIndex], testChunks[testChunkIndex]
+        
     #Run the kNearestNeighbor algorithm with euclidean distance
+    predy, calcTime, predTime = knnEuclideanClassifier(trainX, trainy, testX, numNeighbors)
     
+    print("Time of fitting data: ", calcTime, " seconds")
+    print("Time of prediciting data: ", predTime, " seconds")
     
+    #Creating and plotting the confusion matrix
+    confusionMatrix = metrics.confusion_matrix(testy, predy)
+    dispConfusionMatrix = metrics.ConfusionMatrixDisplay(confusionMatrix)
+    dispConfusionMatrix.plot()
+    plt.show()
+    #Creating and plotting the error rate
+    errorRate = metrics.accuracy_score(testy, predy)
+    print("Error Rate of the classification: ", errorRate)
     
+    #Identify and plot some of the misclassifications
+    misclassified = np.where(testy != predy)[0]
+    misclassifiedTrue, misclassifiedFalse = [],[]
+    for i in range(min(len(misclassified), numPlotImages)):  
+        misclassifiedImage = testX[misclassified[i]]
+        misclassifiedImage = misclassifiedImage.reshape(imageW,imageH)
+        plt.subplot(330 + 1 + i)
+        plt.imshow(misclassifiedImage, cmap=plt.get_cmap('gray'))
+        misclassifiedTrue.append(testy[misclassified[i]])
+        misclassifiedFalse.append(predy[misclassified[i]])
     
+    print("Actual values of misclassified digits: ", misclassifiedTrue)
+    print("Predicted values of misclassified digits: ", misclassifiedFalse)    
+    plt.show()
     
+    #Identify and plot some of the correct classifications
+    correctClassified = np.where(testy == predy)[0]
+    classifiedTrue = []
+    for i in range(min(len(correctClassified), numPlotImages)):  
+        classifiedImage = testX[correctClassified[i]]
+        classifiedImage = classifiedImage.reshape(imageW,imageH)
+        plt.subplot(330 + 1 + i)
+        plt.imshow(classifiedImage, cmap=plt.get_cmap('gray'))
+        classifiedTrue.append(predy[correctClassified[i]])
+        
+    print("Correct classifications: ", classifiedTrue)
+    plt.show()
     
-    
-    
-    
-    pyplot.imshow(trainX[0], cmap=pyplot.get_cmap('gray'))
-    pyplot.show()
-    
-
-    pyplot.show()
-
-main()
